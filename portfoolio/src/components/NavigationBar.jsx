e5302409c514fe67dee49a97b13b71e0fb96355b
Raiko-Rajalaane <raiko.rajalaane@gmail.com>
2025-05-06 00:55:02 +0300

    text centered

--- portfoolio/src/components/NavigationBar.jsx
+++ portfoolio/src/components/NavigationBar.jsx
@@ -10,7 +10,7 @@ function NavigationBar() {
         <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
--- portfoolio/src/pages/Home.jsx
+++ portfoolio/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 
 function Home() {
   return (
-    <div className="text-center">
-      <h1>Hi, I'm Raiko Rajalaane</h1>
-      <p className="lead">Welcome to my portfolio. I'll add projects soon...</p>
+    <div 
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ minHeight: '80vh' }}
+    >
+      <h1 className="text-center">Hi, I'm Raiko Rajalaane</h1>
+      <p className="lead text-center">Welcome to my portfolio. I'll add projects soon...</p>
     </div>
   )
 }
